5718af93b1eae454976d86ac945e29307eed74e5
EgorZhuganov <egor-zhuganov@yandex.ru>
2021-12-30 15:12:54 +1000

    improvement: добавить в RealTask конструктор

--- src/main/java/com/ezh/taskbook/task/RealTask.java
+++ src/main/java/com/ezh/taskbook/task/RealTask.java
@@ -2,4 +2,9 @@ package main.java.com.ezh.taskbook.task;
 
 public class RealTask extends AbstractTask {
 
+    public RealTask() {
+        setStatus(StatusTask.NEW);
+        setName(getName());
+        setDescription(getDescription());
+    }
 }
