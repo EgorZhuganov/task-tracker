40aafb76d3dec7371d7a2ea95848da11d0b5c7a1
EgorZhuganov <egor-zhuganov@yandex.ru>
2021-12-30 15:13:11 +1000

    improvement: добавить в Subtask конструктор

--- src/main/java/com/ezh/taskbook/task/Subtask.java
+++ src/main/java/com/ezh/taskbook/task/Subtask.java
@@ -2,4 +2,9 @@ package main.java.com.ezh.taskbook.task;
 
 public class Subtask extends AbstractTask {
 
+    public Subtask() {
+        setStatus(StatusTask.NEW);
+        setName(getName());
+        setDescription(getDescription());
+    }
 }
