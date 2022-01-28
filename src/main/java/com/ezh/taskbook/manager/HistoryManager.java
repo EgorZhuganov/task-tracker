4caabef8b8f2d064db236fab34524e4124662f87
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-01-29 01:11:39 +1000

    feat: добавить функциональность по просмотру истории задач

---
+++ src/main/java/com/ezh/taskbook/manager/HistoryManager.java
@@ -0,0 +1,18 @@
+package com.ezh.taskbook.manager;
+
+import com.ezh.taskbook.task.AbstractTask;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface HistoryManager {
+
+    void add(AbstractTask task);
+
+    void remove(UUID id);
+
+    List<AbstractTask> getHistory();
+
+    void cleanHistory();
+
+}
