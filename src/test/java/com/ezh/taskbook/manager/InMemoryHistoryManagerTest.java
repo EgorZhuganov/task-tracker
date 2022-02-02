538110f063a29b86ee772388f3a903f221c2580b
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-02-02 19:40:32 +1000

    test: добавить и изменить тесты класса InMemoryHistoryManager

--- src/test/java/com/ezh/taskbook/manager/InMemoryHistoryManagerTest.java
+++ src/test/java/com/ezh/taskbook/manager/InMemoryHistoryManagerTest.java
@@ -1,96 +1,227 @@
 package com.ezh.taskbook.manager;
 
+import com.ezh.taskbook.task.AbstractTask;
 import com.ezh.taskbook.task.Epic;
 import com.ezh.taskbook.task.SingleTask;
 import com.ezh.taskbook.task.Subtask;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 class InMemoryHistoryManagerTest {
 
-    TaskManager manager = new Managers().getDefault();
-
     @Test
-    void add() {
+    void add() throws IllegalAccessException, NoSuchFieldException {
+
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+        Class<? extends InMemoryHistoryManager> ihm = historyManager.getClass();
+        Field field = ihm.getDeclaredField("historyLastTenTasks");
+        field.setAccessible(true);
+        Map<?, ?> hashMap = (HashMap<?, ?>) field.get(historyManager);
+
         Epic epic1 = new Epic();
         Epic epic2 = new Epic();
         Epic epic3 = new Epic();
 
-        epic1.setName("FirstEpic");
-        epic2.setName("SecondEpic");
-        epic3.setName("ThirdEpic");
+        historyManager.add(epic1);
+        historyManager.add(epic3);
+        historyManager.add(epic2);
 
-        manager.addEpic(epic1);
-        manager.addEpic(epic2);
-        manager.addEpic(epic3);
-
-        manager.getEpicByUuid(epic1.getUuid());
-        manager.getEpicByUuid(epic2.getUuid());
-        manager.getEpicByUuid(epic3.getUuid());
-        manager.getEpicByUuid(epic1.getUuid());
-        manager.getEpicByUuid(epic2.getUuid());
-
+        Assertions.assertEquals(3, hashMap.size());
     }
 
     @Test
-    void remove() {
+    void remove() throws NoSuchFieldException, IllegalAccessException {
+
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+        Class<? extends InMemoryHistoryManager> ihm = historyManager.getClass();
+        Field field = ihm.getDeclaredField("historyLastTenTasks");
+        field.setAccessible(true);
+        Map<?, ?> hashMap = (HashMap<?, ?>) field.get(historyManager);
+
         Epic epic1 = new Epic();
         Epic epic2 = new Epic();
         Epic epic3 = new Epic();
         Epic epic4 = new Epic();
 
-        epic1.setName("FirstEpic");
-        epic2.setName("SecondEpic");
-        epic3.setName("ThirdEpic");
-        epic4.setName("FourthEpic");
+        historyManager.add(epic1);
+        historyManager.add(epic2);
+        historyManager.add(epic3);
+        historyManager.add(epic4);
 
-        manager.addEpic(epic1);
-        manager.addEpic(epic2);
-        manager.addEpic(epic3);
-        manager.addEpic(epic4);
+        historyManager.remove(epic2.getUuid());
 
-        manager.getEpicByUuid(epic1.getUuid());
-        manager.getEpicByUuid(epic2.getUuid());
-        manager.getEpicByUuid(epic3.getUuid());
-        manager.getEpicByUuid(epic4.getUuid());
+        Assertions.assertEquals(3, hashMap.size());
+        Assertions.assertEquals(3, historyManager.getHistory().size());
+    }
 
-        manager.removeTaskFromHistory(epic2.getUuid());
+    @Test
+    void removeNode() throws NoSuchFieldException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
-        Assertions.assertEquals(3, manager.getHistory().size());
-        System.out.println("Excellent");
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+        Class<? extends InMemoryHistoryManager> ihm = historyManager.getClass();
+        Class<?> node = InMemoryHistoryManager.class.getDeclaredClasses()[0];
+
+        Method methodRemoveNode = historyManager.getClass().getDeclaredMethod("removeNode", node);
+        var tail = ihm.getDeclaredField("tail");
+        var head = ihm.getDeclaredField("head");
+        Method methodLinkLast = historyManager.getClass().getDeclaredMethod("linkLast", AbstractTask.class);
+
+        tail.setAccessible(true);
+        head.setAccessible(true);
+        methodRemoveNode.setAccessible(true);
+        methodLinkLast.setAccessible(true);
+
+        Epic epic1 = new Epic();
+        Epic epic2 = new Epic();
+        Epic epic3 = new Epic();
+        Epic epic4 = new Epic();
+
+        methodLinkLast.invoke(historyManager, epic3);
+        methodLinkLast.invoke(historyManager, epic4);
+        methodLinkLast.invoke(historyManager, epic2);
+        methodLinkLast.invoke(historyManager, epic1);
+
+        var tailNode = tail.get(historyManager);
+        var prevTailNode = tailNode.getClass().getDeclaredField("prev");
+        var nextTailNode = tailNode.getClass().getDeclaredField("next");
+
+        var headNode = head.get(historyManager);
+        var prevHeadNode = headNode.getClass().getDeclaredField("prev");
+        var nextHeadNode = headNode.getClass().getDeclaredField("next");
+
+        var prevNode = prevTailNode.get(tailNode);
+        var nextNode = nextHeadNode.get(headNode);
+
+        methodRemoveNode.invoke(historyManager, prevNode);
+        methodRemoveNode.invoke(historyManager, nextNode);
+
+        Assertions.assertNull(nextTailNode.get(tailNode));
+        Assertions.assertNull(prevHeadNode.get(headNode));
+        Assertions.assertEquals(headNode, prevTailNode.get(tailNode));
+
+    }
+
+    @Test
+    void linkLast() throws
+            NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+        Class<? extends InMemoryHistoryManager> ihm = historyManager.getClass();
+
+        Method methodLinkLast = historyManager.getClass().getDeclaredMethod("linkLast", AbstractTask.class);
+        Field tail = ihm.getDeclaredField("tail");
+        Field head = ihm.getDeclaredField("head");
+
+        tail.setAccessible(true);
+        head.setAccessible(true);
+        methodLinkLast.setAccessible(true);
+
+        Epic epic1 = new Epic();
+        Epic epic2 = new Epic();
+        Epic epic3 = new Epic();
+
+        Assertions.assertNull(tail.get(historyManager));
+        Assertions.assertNull(head.get(historyManager));
+
+        methodLinkLast.invoke(historyManager, epic1);
+        methodLinkLast.invoke(historyManager, epic3);
+        methodLinkLast.invoke(historyManager, epic2);
+        methodLinkLast.invoke(historyManager, epic1);
+
+        Assertions.assertNotNull(tail.get(historyManager));
+        Assertions.assertNotNull(head.get(historyManager));
+
+        var tailNode = tail.get(historyManager);
+        var tailElement = tailNode.getClass().getDeclaredField("element");
+        var prevTailNode = tailNode.getClass().getDeclaredField("prev");
+        var nextTailNode = tailNode.getClass().getDeclaredField("next");
+
+        var headNode = head.get(historyManager);
+        var headElement = headNode.getClass().getDeclaredField("element");
+        var prevHeadNode = headNode.getClass().getDeclaredField("prev");
+        var nextHeadNode = headNode.getClass().getDeclaredField("next");
+
+        var firstTask = (AbstractTask) headElement.get(headNode);
+        var lastTask = (AbstractTask) tailElement.get(tailNode);
+
+        var prevNode = prevTailNode.get(tailNode);
+        var prevFieldElement = prevNode.getClass().getDeclaredField("element");
+        var prevElement = (AbstractTask) prevFieldElement.get(prevNode);
+
+        var nextNode = nextHeadNode.get(headNode);
+        var nextFieldElement = headNode.getClass().getDeclaredField("element");
+        var nextElement = (AbstractTask) nextFieldElement.get(nextNode);
+
+        Assertions.assertNull(nextTailNode.get(tailNode));
+        Assertions.assertNull(prevHeadNode.get(headNode));
+        Assertions.assertEquals(epic1.getUuid(), firstTask.getUuid());
+        Assertions.assertEquals(epic1.getUuid(), lastTask.getUuid());
+        Assertions.assertEquals(epic2.getUuid(), prevElement.getUuid());
+        Assertions.assertEquals(epic3.getUuid(), nextElement.getUuid());
+
+        methodLinkLast.invoke(historyManager, epic3);   //look change or not tail
+        var tailNodeAfterFirstCheck = tail.get(historyManager);
+        var tailElementAfterFirstCheck = tailNodeAfterFirstCheck.getClass().getDeclaredField("element");
+        tailElementAfterFirstCheck.setAccessible(true);
+        var lastTask2 = (AbstractTask) tailElementAfterFirstCheck.get(tailNodeAfterFirstCheck);
+        Assertions.assertEquals(epic3.getUuid(), lastTask2.getUuid());
     }
 
     @Test
     void getHistory() {
+
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+
+        SingleTask singleTask1 = new SingleTask();
+        SingleTask singleTask2 = new SingleTask();
+
+        Subtask subtask1 = new Subtask();
+        Subtask subtask2 = new Subtask();
+        Subtask subtask3 = new Subtask();
+        Subtask subtask4 = new Subtask();
+        Subtask subtask5 = new Subtask();
+        Subtask subtask6 = new Subtask();
+        Subtask subtask7 = new Subtask();
+        Subtask subtask8 = new Subtask();
+
         Epic epic1 = new Epic();
         Epic epic2 = new Epic();
-        Epic epic3 = new Epic();
-        Epic epic4 = new Epic();
 
-        epic1.setName("FirstEpic");
-        epic2.setName("SecondEpic");
-        epic3.setName("ThirdEpic");
-        epic4.setName("FourthEpic");
+        historyManager.add(singleTask2);
+        historyManager.add(epic1);
+        historyManager.add(epic2);
+        historyManager.add(singleTask1);
+        historyManager.add(subtask3);
+        historyManager.add(subtask4);
+        historyManager.add(subtask7);
+        historyManager.add(subtask1);
+        historyManager.add(subtask2);
+        historyManager.add(subtask8);
+        historyManager.add(subtask5);
+        historyManager.add(subtask7);
+        historyManager.add(subtask6);
 
-        manager.addEpic(epic1);
-        manager.addEpic(epic2);
-        manager.addEpic(epic3);
-        manager.addEpic(epic4);
+        Assertions.assertEquals(10, historyManager.getHistory().size());
+        Assertions.assertEquals(historyManager.getHistory().get(0).getUuid(), epic2.getUuid());
+        Assertions.assertEquals(historyManager.getHistory().get(8).getUuid(), subtask7.getUuid());
 
-        manager.getEpicByUuid(epic1.getUuid());
-        manager.getEpicByUuid(epic2.getUuid());
-        manager.getEpicByUuid(epic3.getUuid());
-        manager.getEpicByUuid(epic4.getUuid());
-        manager.getEpicByUuid(epic1.getUuid());
-
-        Assertions.assertEquals(4, manager.getHistory().size());
-
-        System.out.println(manager.getHistory());
-        System.out.println("Excellent");
     }
 
     @Test
-    void cleanHistory() { //в тест попадает 12 тасок, первые две должны удалиться, т.к. max - 10
+    void cleanHistory() throws NoSuchFieldException, IllegalAccessException {
+
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+        Class<? extends InMemoryHistoryManager> ihm = historyManager.getClass();
+        Field field = ihm.getDeclaredField("historyLastTenTasks");
+        field.setAccessible(true);
+        Map<?, ?> hashMap = (HashMap<?, ?>) field.get(historyManager);
+
         Epic epic1 = new Epic();
         Epic epic2 = new Epic();
 
@@ -106,41 +237,24 @@ class InMemoryHistoryManagerTest {
         Subtask subtask7 = new Subtask();
         Subtask subtask8 = new Subtask();
 
-        epic1.setName("FirstEpicWithoutSub");
-        epic2.setName("SecondEpicWithSub");
+        historyManager.add(epic1);
+        historyManager.add(epic2);
+        historyManager.add(singleTask2);
+        historyManager.add(singleTask1);
 
-        singleTask1.setName("FirstSingleTask");
-        singleTask2.setName("SecondSingleTask");
+        historyManager.add(subtask8);
+        historyManager.add(subtask7);
+        historyManager.add(subtask6);
+        historyManager.add(subtask5); //call two time
+        historyManager.add(subtask4); //call two time
+        historyManager.add(subtask3);
+        historyManager.add(subtask2);
+        historyManager.add(subtask1);
+        historyManager.add(subtask4);
+        historyManager.add(subtask5);
 
-        subtask1.setName("FirstSub");
-        subtask2.setName("SecondSub");
-        subtask3.setName("ThirdSub");
-        subtask4.setName("FourthSub");
-        subtask5.setName("FifthSub");
-        subtask6.setName("SixthSub");
-        subtask7.setName("SeventhSub");
-        subtask8.setName("EighthSub");
-
-        manager.addEpicWithSubtask(epic1, subtask4, subtask5, subtask6, subtask7);
-        manager.addEpicWithSubtask(epic2, subtask1, subtask2, subtask3, subtask8);
-        manager.addSingleTask(singleTask1);
-        manager.addSingleTask(singleTask2);
-
-        manager.getEpicByUuid(epic1.getUuid());
-        manager.getEpicByUuid(epic2.getUuid()); //таска уйдет из списка просмотров, т.к. она первая с головы (эпик 1 не первый - т.к. был повторный вызов)
-        manager.getSingleTaskByUuid(singleTask1.getUuid()); //становится первой с головы и тоже уходит из списка
-        manager.getSingleTaskByUuid(singleTask2.getUuid());
-        manager.getEpicByUuid(epic1.getUuid()); //повторный вызов эпик1, первый вызов удаляется и переносится
-        manager.getSubtaskByUuid(subtask2.getUuid());
-        manager.getSubtaskByUuid(subtask1.getUuid());
-        manager.getSubtaskByUuid(subtask3.getUuid());
-        manager.getSubtaskByUuid(subtask4.getUuid());
-        manager.getSubtaskByUuid(subtask7.getUuid());
-        manager.getSubtaskByUuid(subtask8.getUuid());
-        manager.getSubtaskByUuid(subtask5.getUuid());
-        manager.getSubtaskByUuid(subtask6.getUuid());
-
-        Assertions.assertEquals(10, manager.getHistory().size());
-        System.out.println(manager.getHistory());
+        Assertions.assertEquals(10, hashMap.size()); //call all tasks (12), but size always 10
+        Assertions.assertNull(hashMap.get(epic1));
+        Assertions.assertNull(hashMap.get(epic2));
     }
 }
