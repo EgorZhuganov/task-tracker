c2ddd29989895693bffbd135932146dd986d7551
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-03-05 23:54:51 +1000

    feat: добавить сериализацию и десериализацию в HistoryManager

--- src/main/java/com/ezh/taskbook/manager/HistoryManager.java
+++ src/main/java/com/ezh/taskbook/manager/HistoryManager.java
@@ -2,6 +2,7 @@ package com.ezh.taskbook.manager;
 
 import com.ezh.taskbook.task.AbstractTask;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,4 +14,18 @@ public interface HistoryManager {
 
     List<AbstractTask> getHistory();
 
+    static String asString(HistoryManager manager) {
+        StringBuilder sb = new StringBuilder();
+        manager.getHistory().forEach(abstractTask -> sb.append(abstractTask.getUuid().toString()).append(";"));
+        return sb.toString();
+    }
+
+    static List<UUID> fromString(String value){
+        List<UUID> uuidList = new ArrayList<>();
+        String[] uuidFields = value.split(";");
+        for (String uuid : uuidFields) {
+            uuidList.add(UUID.fromString(uuid));
+        }
+        return uuidList;
+    }
 }
