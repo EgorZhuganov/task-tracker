e44772a9cbfff64213a99d9e5ffabf5a1df42abf
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-03-05 14:44:46 +1000

    feat: добавить классы ManagerRestoreException и ManagerSaveException

---
+++ src/main/java/com/ezh/taskbook/exception/ManagerRestoreException.java
@@ -0,0 +1,11 @@
+package com.ezh.taskbook.exception;
+
+public class ManagerRestoreException extends RuntimeException {
+
+    public ManagerRestoreException() {
+    }
+
+    public ManagerRestoreException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/ezh/taskbook/exception/ManagerSaveException.java
@@ -0,0 +1,11 @@
+package com.ezh.taskbook.exception;
+
+public class ManagerSaveException extends RuntimeException {
+
+    public ManagerSaveException() {
+    }
+
+    public ManagerSaveException(String message) {
+        super(message);
+    }
+}
