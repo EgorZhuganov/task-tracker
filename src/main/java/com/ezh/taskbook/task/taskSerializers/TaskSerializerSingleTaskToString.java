20fa7a3044ffc51b67a8fb489759c7349b96cb3c
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-03-22 02:46:24 +1000

    feat: добавить в TaskSerializerSingleTaskToString и TaskSerializerSubtaskToString сериализацию новых полей

--- src/main/java/com/ezh/taskbook/task/taskSerializers/TaskSerializerSingleTaskToString.java
+++ src/main/java/com/ezh/taskbook/task/taskSerializers/TaskSerializerSingleTaskToString.java
@@ -1,16 +1,30 @@
 package com.ezh.taskbook.task.taskSerializers;
 
+import com.ezh.taskbook.task.TypeTask;
 import com.ezh.taskbook.task.*;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class TaskSerializerSingleTaskToString extends TaskSerializerToString<AbstractTask> {
+
+    @Override
+    public String taskAsString(AbstractTask task) {
+        var sb = new StringBuilder(super.taskAsString(task)).
+                append(task.getStartTime()).append(CSV_SEPARATOR).
+                append(task.getDuration()).append(CSV_SEPARATOR);
+        return sb.toString();
+    }
 
     @Override
     public SingleTask taskFromString(String value) {
         SingleTask singleTask = (SingleTask) super.taskFromString(value);
         String[] fields = value.split(CSV_SEPARATOR);
-        singleTask.setDescription(fields[4]);
         singleTask.setName(fields[2]);
         singleTask.setStatus(StatusTask.valueOf(fields[3]));
+        singleTask.setDescription(fields[4]);
+        singleTask.setStartTime(LocalDateTime.parse(fields[5]));
+        singleTask.setDuration(Duration.parse(fields[6]));
         return singleTask;
     }
 
--- src/main/java/com/ezh/taskbook/task/taskSerializers/TaskSerializerSubtaskToString.java
+++ src/main/java/com/ezh/taskbook/task/taskSerializers/TaskSerializerSubtaskToString.java
@@ -1,13 +1,19 @@
 package com.ezh.taskbook.task.taskSerializers;
 
+import com.ezh.taskbook.task.TypeTask;
 import com.ezh.taskbook.task.*;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class TaskSerializerSubtaskToString extends TaskSerializerToString<AbstractTask> {
 
     @Override
     public String taskAsString(AbstractTask task) {
-        var sb = new StringBuilder(super.taskAsString(task))
-                .append(((Subtask)task).getEpic().getUuid());
+        var sb = new StringBuilder(super.taskAsString(task)).
+                append(task.getStartTime()).append(CSV_SEPARATOR).
+                append(task.getDuration()).append(CSV_SEPARATOR).
+                append(((Subtask)task).getEpic().getUuid());
         return sb.toString();
     }
 
@@ -18,7 +24,10 @@ public class TaskSerializerSubtaskToString extends TaskSerializerToString<Abstra
         subtask.setName(fields[2]);
         subtask.setStatus(StatusTask.valueOf(fields[3]));
         subtask.setDescription(fields[4]);
-        subtask.setEpic(new Epic(fields[5]));
+        subtask.setStartTime(LocalDateTime.parse(fields[5]));
+        subtask.setDuration(Duration.parse(fields[6]));
+        subtask.setEpic(new Epic(fields[7]));
+
         return subtask;
     }
 
