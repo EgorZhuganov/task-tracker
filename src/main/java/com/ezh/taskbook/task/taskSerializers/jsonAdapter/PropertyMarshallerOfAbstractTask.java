817af319b57eecf1787714494466836b48df5f82
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-07 18:51:07 +1000

    feat: добавить PropertyMarshallerOfAbstractTask для сериализации List<AbstractTask> в/из Json

---
+++ src/main/java/com/ezh/taskbook/task/taskSerializers/jsonAdapter/PropertyMarshallerOfAbstractTask.java
@@ -0,0 +1,37 @@
+package com.ezh.taskbook.task.taskSerializers.jsonAdapter;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+
+public class PropertyMarshallerOfAbstractTask implements JsonSerializer<Object>, JsonDeserializer<Object> {
+
+    private static final String CLASS_TYPE = "CLASS_TYPE";
+
+    @Override
+    public Object deserialize(JsonElement jsonElement,
+                              Type type,
+                              JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+
+        JsonObject jsonObj = jsonElement.getAsJsonObject();
+        String className = jsonObj.get(CLASS_TYPE).getAsString();
+
+        try {
+            Class<?> clz = Class.forName(className);
+            return jsonDeserializationContext.deserialize(jsonElement, clz);
+        } catch (ClassNotFoundException e) {
+            throw new JsonParseException(e);
+        }
+    }
+
+    @Override
+    public JsonElement serialize(Object object,
+                                 Type type,
+                                 JsonSerializationContext jsonSerializationContext) {
+        Gson gson = new Gson();
+        gson.toJson(object, object.getClass());
+        JsonElement jsonElement = gson.toJsonTree(object);
+        jsonElement.getAsJsonObject().addProperty(CLASS_TYPE, object.getClass().getCanonicalName());
+        return jsonElement;
+    }
+}
