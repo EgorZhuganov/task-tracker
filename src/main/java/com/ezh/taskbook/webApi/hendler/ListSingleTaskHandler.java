42124374cd122b4f83c0557bd8254c2e06ef7d2f
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-09 00:16:40 +1000

    change: изменить метод handle

--- src/main/java/com/ezh/taskbook/webApi/hendler/ListSingleTaskHandler.java
+++ src/main/java/com/ezh/taskbook/webApi/hendler/ListSingleTaskHandler.java
@@ -1,10 +1,12 @@
 package com.ezh.taskbook.webApi.hendler;
 
 import com.ezh.taskbook.manager.TaskManager;
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class ListSingleTaskHandler implements HttpHandler {
 
@@ -16,21 +18,27 @@ public class ListSingleTaskHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String method = exchange.getRequestMethod();
-        switch (method) {
-            case "GET":
-                manager.getListSingleTasks();
-                System.out.println("Single tasks were got");
-                exchange.sendResponseHeaders(200, 0);
-                break;
-            case "DELETE":
-                manager.clearSingleTasks();
-                System.out.println("Single tasks were deleted");
-                exchange.sendResponseHeaders(204, -1);
-                break;
-            default:
-                System.out.println("This context work only with methods: GET, DELETE");
-                exchange.sendResponseHeaders(400,-1);
+        try {
+            String method = exchange.getRequestMethod();
+            switch (method) {
+                case "GET":
+                    manager.getListSingleTasks();
+                    String response = new Gson().toJson(manager.getListSingleTasks());
+                    exchange.sendResponseHeaders(200, response.length());
+                    exchange.getResponseBody().write(response.getBytes(StandardCharsets.UTF_8));
+                    System.out.println("Single tasks were got");
+                    break;
+                case "DELETE":
+                    manager.clearSingleTasks();
+                    System.out.println("Single tasks were deleted");
+                    exchange.sendResponseHeaders(204, -1);
+                    break;
+                default:
+                    System.out.println("This context work only with methods: GET, DELETE");
+                    exchange.sendResponseHeaders(400, -1);
+            }
+        } finally {
+            exchange.close();
         }
     }
 }
