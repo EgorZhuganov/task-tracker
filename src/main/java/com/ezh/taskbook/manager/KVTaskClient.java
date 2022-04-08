737826aeee0e638fa39617513c6bf0cc0e79e3b4
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-08 17:42:54 +1000

    feat: добавить класс KVTaskClient

---
+++ src/main/java/com/ezh/taskbook/manager/KVTaskClient.java
@@ -0,0 +1,66 @@
+package com.ezh.taskbook.manager;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class KVTaskClient {
+
+    private static String SERVER_KEY;
+    private HttpRequest request;
+    private HttpClient client;
+    private URI url;
+
+    public KVTaskClient(URI serverUrl) throws IOException {
+        client = HttpClient.newHttpClient();
+        url = serverUrl;
+        register();
+    }
+
+    private void register() {
+        String pathToRegister = url.toString().concat("register");
+        URI path = URI.create(pathToRegister);
+        request = HttpRequest.newBuilder()
+                .GET()
+                .uri(path)
+                .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            SERVER_KEY = response.body();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void put(String key, String json) {
+        String pathToSave = url.toString().concat(String.format("save/%s?API_KEY=%s", key, SERVER_KEY));
+        URI path = URI.create(pathToSave);
+        request = HttpRequest.newBuilder()
+                .uri(path)
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+        try {
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String load(String key) {
+        String response = "";
+        String pathToSave = url.toString().concat(String.format("load/%s?API_KEY=%s", key, SERVER_KEY));
+        URI path = URI.create(pathToSave);
+        request = HttpRequest.newBuilder()
+                .uri(path)
+                .GET()
+                .build();
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+}
