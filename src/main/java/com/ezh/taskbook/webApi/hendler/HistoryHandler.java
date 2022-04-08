9f5ef9e978f72dbe44d4e9ad89478be94b71be37
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-08 20:39:37 +1000

    change: изменить HistoryHandler

--- src/main/java/com/ezh/taskbook/webApi/hendler/HistoryHandler.java
+++ src/main/java/com/ezh/taskbook/webApi/hendler/HistoryHandler.java
@@ -1,14 +1,16 @@
 package com.ezh.taskbook.webApi.hendler;
 
 import com.ezh.taskbook.manager.TaskManager;
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class HistoryHandler implements HttpHandler {
 
-    TaskManager manager;
+    private final TaskManager manager;
 
     public HistoryHandler (TaskManager manager) {
         this.manager = manager;
@@ -16,16 +18,30 @@ public class HistoryHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String method = exchange.getRequestMethod();
-        switch (method) {
-            case "GET":
-                manager.getHistory();
-                System.out.println("History was got");
-                exchange.sendResponseHeaders(200, 0);
-                break;
-            default:
-                System.out.println("This context work only with method GET");
-                exchange.sendResponseHeaders(400,-1);
+        try {
+            String method = exchange.getRequestMethod();
+            switch (method) {
+                case "GET":
+                    if (manager.getHistory().size() == 0) {
+                        //TODO нужно отправлять такой ответ или нет? Какой ответ отправлять если данных нет? Писать ли что-то в боди? Или 204-ый код?
+                        String response = "No data to transfer";
+                        exchange.sendResponseHeaders(200, response.length());
+                        exchange.getResponseBody().write(response.getBytes(StandardCharsets.UTF_8));
+                        System.out.println(response);
+                    } else {
+                        manager.getHistory();
+                        String response = new Gson().toJson(manager.getHistory());
+                        exchange.sendResponseHeaders(200, response.length());
+                        exchange.getResponseBody().write(response.getBytes(StandardCharsets.UTF_8));
+                        System.out.println("History was got");
+                    }
+                    break;
+                default:
+                    System.out.println("This context work only with method GET");
+                    exchange.sendResponseHeaders(400, -1);
+            }
+        } finally {
+            exchange.close();
         }
     }
 }
