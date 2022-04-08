0100552b28139ed01a7f200f8fc8fee480044528
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-08 20:40:08 +1000

    test: добавить тест для HistoryHandler

---
+++ src/test/java/com/ezh/taskbook/webApi/hendler/HistoryHandlerTest.java
@@ -0,0 +1,102 @@
+package com.ezh.taskbook.webApi.hendler;
+
+import com.ezh.taskbook.exception.TaskNotFoundException;
+import com.ezh.taskbook.exception.TasksIntersectionException;
+import com.ezh.taskbook.manager.FileBackedTasksManager;
+import com.ezh.taskbook.manager.TaskManager;
+import com.ezh.taskbook.task.SingleTask;
+import com.ezh.taskbook.webApi.HttpTaskServer;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HistoryHandlerTest {
+
+    private final TaskManager manager = new FileBackedTasksManager(new File("test.txt"));
+    private final HttpTaskServer server = new HttpTaskServer(manager, 8080);
+    private final HttpClient client = HttpClient.newHttpClient();
+    private HttpRequest request;
+    private final URI url = URI.create("http://localhost:8080/tasks/history");
+
+    @BeforeEach
+    public void beforeEach() { server.start(); }
+
+    @AfterEach
+    public void afterEach() {
+        server.stop();
+    }
+
+    @Test //GET
+    public void test1_checkContextWithGetRequestIfAddTwoSingleTaskToHistoryShouldReturnStatusCode200AndHistoryAsJson ()
+            throws TasksIntersectionException, TaskNotFoundException, IOException, InterruptedException {
+        SingleTask singleTask1 = new SingleTask();
+        SingleTask singleTask2 = new SingleTask();
+
+        manager.addSingleTask(singleTask1);
+        manager.addSingleTask(singleTask2);
+        manager.getSingleTaskByUuid(singleTask1.getUuid());
+        manager.getSingleTaskByUuid(singleTask2.getUuid());
+
+        request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String historyToJson = new Gson().toJson(manager.getHistory());
+
+        Assertions.assertEquals(historyToJson, response.body());
+        Assertions.assertEquals(200, response.statusCode());
+    }
+
+    @Test //GET
+    public void test2_checkContextWithGetRequestIfThereIsNotAnyTasksInHistoryShouldReturnCode201 ()
+            throws TasksIntersectionException, IOException, InterruptedException {
+        SingleTask singleTask1 = new SingleTask();
+        SingleTask singleTask2 = new SingleTask();
+
+        manager.addSingleTask(singleTask1);
+        manager.addSingleTask(singleTask2);
+
+        request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertEquals("No data to transfer", response.body());
+        Assertions.assertEquals(200, response.statusCode());
+    }
+
+    @Test //DELETE - NOT EXIST
+    public void test3_checkContextWithDeleteRequestWhichNotExist()
+            throws TasksIntersectionException, IOException, InterruptedException {
+        SingleTask singleTask1 = new SingleTask();
+        SingleTask singleTask2 = new SingleTask();
+
+        manager.addSingleTask(singleTask1);
+        manager.addSingleTask(singleTask2);
+
+        request = HttpRequest.newBuilder()
+                .uri(url)
+                .DELETE()
+                .build();
+
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertEquals("", response.body());
+        Assertions.assertEquals(400, response.statusCode());
+    }
+}
