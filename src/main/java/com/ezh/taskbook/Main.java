890326a917ff0e285b4ffc54a55d942cf201b032
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-10 04:57:29 +1000

    style: добавить 2 Main (без функциональности)

--- src/main/java/com/ezh/taskbook/Main.java
+++ src/main/java/com/ezh/taskbook/Main.java
@@ -1,13 +1,28 @@
 package com.ezh.taskbook;
 
-import com.ezh.taskbook.manager.Managers;
-import com.ezh.taskbook.manager.TaskManager;
+import com.ezh.taskbook.manager.HttpTaskManager;
+import com.ezh.taskbook.webApi.HttpTaskServer;
+
+import java.net.URI;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        TaskManager manager = new Managers().getDefault();
+        HttpTaskServer myServer1 = new HttpTaskServer(
+                new HttpTaskManager(URI.create("http://localhost:8078/"), "8080"), 8080);
+        myServer1.start();
 
+        HttpTaskServer myServer2 = new HttpTaskServer(
+                new HttpTaskManager(URI.create("http://localhost:8078/"), "8081"), 8081);
+        myServer2.start();
+
+        HttpTaskServer myServer3 = new HttpTaskServer(
+                new HttpTaskManager(URI.create("http://localhost:8078/"), "8082"), 8082);
+        myServer3.start();
+
+        myServer1.stop();
+        myServer2.stop();
+        myServer3.stop();
     }
 }
--- src/main/java/com/ezh/taskbook/kvserver/Main.java
+++ src/main/java/com/ezh/taskbook/kvserver/Main.java
@@ -1,15 +1,13 @@
 package com.ezh.taskbook.kvserver;
 
-import com.ezh.taskbook.exception.TaskNotFoundException;
-import com.ezh.taskbook.exception.TasksIntersectionException;
-
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, TasksIntersectionException, TaskNotFoundException {
+    public static void main(String[] args) throws IOException {
 
-        new KVServer().start(); //port 8078
-
+        KVServer kvServer = new KVServer();
+        kvServer.start(); //port 8078
+        kvServer.stop();
     }
 }
