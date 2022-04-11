8a6bae4a46b4341826b0bfd407e04426ffc2772b
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-04-11 14:05:35 +1000

    test: изменить логику теста в связи с изменением возвращемого значения методом getDefault класса Managers

--- src/test/java/com/ezh/taskbook/manager/ManagersTest.java
+++ src/test/java/com/ezh/taskbook/manager/ManagersTest.java
@@ -1,19 +1,25 @@
 package com.ezh.taskbook.manager;
 
 import com.ezh.taskbook.exception.TaskNotFoundException;
+import com.ezh.taskbook.kvserver.KVServer;
 import com.ezh.taskbook.task.Epic;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 class ManagersTest {
 
     @Test
-    void getDefault() throws TaskNotFoundException {
-        TaskManager manager = new Managers().getDefault();
+    void getDefault() throws TaskNotFoundException, IOException {
+        KVServer server = new KVServer();
+        server.start();
+        TaskManager manager = new Managers().getDefault(); //если дефолтное значение это HttpTaskManager
         Epic epic = new Epic();
         manager.addEpic(epic);
         manager.getListEpics();
         System.out.println(manager.getEpicByUuid(epic.getUuid()));
         Assertions.assertEquals(1, manager.getListEpics().size());
+        server.stop();
     }
 }
